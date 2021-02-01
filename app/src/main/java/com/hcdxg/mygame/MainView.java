f1ca0448bc74de8664953a9d37655096e0f2fd74
子毅 董 <dzy7eu7d7@outlook.com>
2021-02-01 23:02:25 +0800

    add customize

--- app/src/main/java/com/hcdxg/mygame/MainView.java
+++ app/src/main/java/com/hcdxg/mygame/MainView.java
@@ -1,20 +1,25 @@
 package com.hcdxg.mygame;
 import com.uxyq7e.test.*;
 import com.uxyq7e.test.tools.*;
+
+import android.content.Context;
 import android.view.*;
+import android.view.inputmethod.InputMethodManager;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
 
 public class MainView extends GameView
 {
 	MyDialog md;
 	boolean diatc;
 	public MainView(){
-		Image title=new Image(345,300,390,100);
+		Image title=new Image(286,300,507,130);
 		title.setbitmap(tool.loadbitmap("title.png"));
 		Button ks=new Button();
 		ks.settextsize(70);
 		ks.settext("开始游戏");
 		ks.setsize(350,150);
-		ks.setposition_center(540,800);
+		ks.setposition_center(540,700);
 		ks.setbitmap(tool.loadbitmap("an.png"));
 		ks.setclick(new Button.ClickListener(){
 				@Override
@@ -33,9 +38,9 @@ public class MainView extends GameView
 			});
 		Button sm=new Button();
 		sm.settextsize(70);
-		sm.settext("说明");
+		sm.settext("自定义");
 		sm.setsize(350,150);
-		sm.setposition_center(540,1000);
+		sm.setposition_center(540,ks.getposition_center().y+200);
 		sm.setbitmap(tool.loadbitmap("an.png"));
 		sm.setclick(new Button.ClickListener(){
 				@Override
@@ -47,7 +52,8 @@ public class MainView extends GameView
 				@Override
 				public boolean up()
 				{
-					Screen.gv=Screen.exp;
+					Screen.gv=Screen.cv;
+					//((InputMethodManager)MainActivity.ma.getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 					return false;
 				}
 			});
@@ -55,7 +61,7 @@ public class MainView extends GameView
 		tc.settextsize(70);
 		tc.settext("退出");
 		tc.setsize(350,150);
-		tc.setposition_center(540,1200);
+		tc.setposition_center(540,sm.getposition_center().y+200);
 		tc.setbitmap(tool.loadbitmap("an.png"));
 		tc.setclick(new Button.ClickListener(){
 				@Override
@@ -75,7 +81,7 @@ public class MainView extends GameView
 		gv.settextsize(70);
 		gv.settext("设置");
 		gv.setsize(350,150);
-		gv.setposition_center(540,1400);
+		gv.setposition_center(540,tc.getposition_center().y+200);
 		gv.setbitmap(tool.loadbitmap("an.png"));
 		gv.setclick(new Button.ClickListener(){
 				@Override
@@ -94,8 +100,8 @@ public class MainView extends GameView
 			});
 		md=new MyDialog();
 		md.setTitle("退出游戏");
-		md.setltext("取消");
-		md.qx.setclick(new Button.ClickListener(){
+		md.setLtext("取消");
+		md.left.setclick(new Button.ClickListener(){
 
 				@Override
 				public boolean down()
@@ -111,8 +117,8 @@ public class MainView extends GameView
 					return false;
 				}
 			});
-		md.setrtext("确定");
-		md.qd.setclick(new Button.ClickListener(){
+		md.setRtext("确定");
+		md.right.setclick(new Button.ClickListener(){
 
 				@Override
 				public boolean down()
